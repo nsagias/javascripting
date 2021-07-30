6edcdb7b3321146c42953d24b6f4362418c1f717
Nick Sagias <nsagias@gmail.com>
2021-07-30 00:21:29 +0000

    Initial commit with my nodeschool work so far

---
+++ numbers-to-string.js
@@ -0,0 +1,4 @@
+let num = 128
+result = 0.0
+result = num.toString()
+console.log(result)
---
+++ pets.js
@@ -0,0 +1,6 @@
+const pets = ['cat', 'dog', 'elephant']  
+       
+const filtered = pets.filter(function (pet) {  
+       return (pet !== 'elephant')  
+     })
+console.log(filtered)
